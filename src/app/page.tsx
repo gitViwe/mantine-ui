b57213a8f4037c0d08aa5cf086560f91c43ecc99
viwe <viwe.nkepu@hotmail.com>
2025-04-17 15:39:15 +0200

    temp content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import {Product} from "@/components/Product/Product";
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
+      <Product />
       <ColorSchemeToggle />
     </>
   );
